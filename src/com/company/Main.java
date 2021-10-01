6b55938da971cd36578003a584e2e2a3017a57b6
shivamface2face <71273850+shivamface2face@users.noreply.github.com>
2021-10-01 12:47:50 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("github addind ");
         System.out.println("again trying it");
+	     System.out.println("again trying it");
+	    
     }
 }
